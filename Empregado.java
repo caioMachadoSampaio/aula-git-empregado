df2ff501e0bf0d66ee91bdab0a82a1512674af58
‘Caio <caiofmsampaio@gmail.com>
2021-03-18 20:47:05 -0300

    mais alteracoes nas duas classes

--- Empregado.java
+++ Empregado.java
@@ -38,4 +38,22 @@ this.tipo = tipo;
 public int tipo (){
 return this.tipo;
 }
+public void setSalario (double salario){
+this.salario = salario;
+}
+public double getSalario (){
+return this.salario;
+}
+public void setComissao (double comissao){
+this.comissao = comissao;
+}
+public double getComissao (){
+return this.comissao;
+}
+public void setBonus (double bonus){
+this.bonus = bonus;
+}
+public double getBonus (){
+return this.bonus;
+}
 }
--- TesteEmpregado.java
+++ TesteEmpregado.java
@@ -5,6 +5,26 @@ Empregado e2 = new Empregado();
 Empregado e3 = new Empregado ();
 //e agora, como configurar o tipo deles?
 //comentario a mais
+//adicionar à classe Empregado.java
+public void setSalario (double salario){
+this.salario = salario;
+}
+public double getSalario (){
+return this.salario;
+}
+public void setComissao (double comissao){
+this.comissao = comissao;
+}
+public double getComissao (){
+return this.comissao;
+}
+public void setBonus (double bonus){
+this.bonus = bonus;
+}
+public double getBonus (){
+return this.bonus;
+}
+
 }
 }
 
